404cb9c36e0df2dc99e612661bf649dfaff67081
Ira S <iryna.shevchenko2@gmail.com>
2024-03-31 17:44:03 +0300

    allowed origins changed

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -1,8 +1,9 @@
 const allowedOrigins = [
-  //   "https://www.yoursite.com",
-  "http://127:0:0:1:3000",
-  "http://localhost:3000",
-  "http://localhost:3000",
+  "https://byebyesyrena.github.io/Medicine-front-end/",
+
+  // "http://127:0:0:1:3000",
+  // "http://localhost:3000",
+  // "http://localhost:3000",
 ];
 
 module.exports = allowedOrigins;
