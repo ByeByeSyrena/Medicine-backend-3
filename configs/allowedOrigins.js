53a57169176ad24bc49d62fd848965f854b235ba
Ira S <iryna.shevchenko2@gmail.com>
2024-03-31 18:34:07 +0300

    test

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "https://byebyesyrena.github.io/Medicine-front-end",
-  "https://byebyesyrena.github.io/",
+  "https://byebyesyrena.github.io",
+
   // "http://127:0:0:1:3000",
   // "http://localhost:3000",
   // "http://localhost:3000",
